bb7807f2abce053caf081081a20009e947962840
Junwei Zheng <junweizheng.cn@outlook.com>
2021-06-17 22:04:47 +0200

    [modified] | add Projection Layer and Decomposer Module

--- model.py
+++ model.py
@@ -67,29 +67,34 @@ class Projection(keras.layers.Layer):
 
     def call(self, inputs):
         x = self.fc(inputs)
-        return x
+        outputs = self.act(x)
+        return outputs
 
 
 class Decomposer(keras.layers.Layer):
 
-    def __init__(self, **kwargs):
+    def __init__(self, num_partrs, **kwargs):
         super(Decomposer, self).__init__(**kwargs)
-        pass
+        self.projection_layer_list = list()
 
-    def build(self, input_shape):
-        pass
+        self.binary_shape_encoder = BinaryShapeEncoder()
+        for i in range(num_partrs):
+            self.projection_layer_list.append(Projection())
 
     def call(self, inputs, training=False):
-        if training:
-            pass
-        else:
-            pass
+        projection_layer_outputs = list()
+        x = self.binary_shape_encoder(inputs, training=training)
+        for each_layer in self.projection_layer_list:
+            projection_layer_outputs.append(each_layer(x))
+        # outputs should be a tensor in the shape of (num_parts, encoding_dimensions)
+        outputs = tf.constant(projection_layer_outputs)
+        return outputs
 
 
-class PartDecoder(keras.layers.Layer):
+class SharedPartDecoder(keras.layers.Layer):
 
     def __init__(self, **kwargs):
-        super(PartDecoder, self).__init__(**kwargs)
+        super(SharedPartDecoder, self).__init__(**kwargs)
         pass
 
     def build(self, input_shape):
@@ -136,29 +141,16 @@ class Model(keras.Model):
 
     def __init__(self, **kwargs):
         super(Model, self).__init__(**kwargs)
-        self.loss_tracker = tf.keras.metrics.Mean()  # 实例化一个metric来记录average training loss
 
     def customized_loss(self):
-        return tf.keras.losses.BinaryCrossentropy(from_logits=True)
+        pass
 
-    def call(self, inputs, training=False):  # 一定要call来定义你的计算图
-        self.x = self.block1(inputs)  # 扩大中间变量x的scope，有利于将来自定义loss的传参
-        y_pred = self.block2(x)  # 如果block2在训练和测试的时候有不同表现，而block1表现相同，只需要在block2传入training=True即可
-        return y_pred
+    def call(self, inputs, training=False):
+        pass
 
     def train_step(self, data):
-        x, y_true = data  # 如果fit(x, y, ...)，那么data参数将会是一个元组(x, y)。类推tf.data.Dataset数据类型
-        with tf.GradientTape() as tape:
-            # 因为重写了train_step，所以调用fit时，不会自动调用call，导致不会自动传入training=True。需要手动调用call以及传入training=True
-            y_pred = self.call(x, training=True)
-            bce_loss = self.customized_loss()(y_true, y_pred) + tf.math.reduce_sum(self.losses)  # 记得加上之前layer的额外自定义loss
-        block1_grads, block2_grads = tape.gradient(bce_loss, [self.block1.trainable_weights, self.block2.trainable_weights])
-        self.optimizer.apply_gradients(zip(block1_grads, self.block1.trainable_weights))
-        self.optimizer.apply_gradients(zip(block2_grads, self.block2.trainable_weights))
-        self.loss_tracker.update_state(bce_loss)  # 计算average training loss，并保存在之前的实例化的loss tracker里面
-        return {'BCE_Loss': self.loss_tracker.result()}  # 这个返回值很重要，它为fit的progress bar以及其他的callback提供了数据
+        pass
 
     @property
     def metrics(self):
-        return [self.loss_tracker]  # 把需要自动reset的tracker放到list里面即可
-
+        pass
