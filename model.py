fa6a9304138eb0d622dcae34c133094ad5520f89
Junwei Zheng <junweizheng.cn@outlook.com>
2021-06-26 01:08:04 +0200

    [modified] | finish implementing Resampling module

--- model.py
+++ model.py
@@ -99,7 +99,7 @@ class SharedPartDecoder(keras.layers.Layer):
         self.fc = layers.Dense(2*2*2*256)
         self.reshape = layers.Reshape((2, 2, 2, 256))
 
-        # inputs should be in the shape of (B, H, W, D, C)
+        # inputs should be in the shape of (B, D, H, W, C)
         self.deconv1 = layers.Conv3DTranspose(128, 3, (2, 2, 2), padding='same', output_padding=(1, 1, 1))
         self.deconv2 = layers.Conv3DTranspose(64, 3, (2, 2, 2), padding='same', output_padding=(1, 1, 1))
         self.deconv3 = layers.Conv3DTranspose(32, 5, (2, 2, 2), padding='same', output_padding=(1, 1, 1))
@@ -137,7 +137,7 @@ class SharedPartDecoder(keras.layers.Layer):
 
         x = self.reshape(x)
 
-        # inputs should be in the shape of (B, H, W, D, C)
+        # inputs should be in the shape of (B, D, H, W, C)
         x = self.deconv1(x)
         x = self.act1(x)
         x = self.bn1(x, training=training)
@@ -161,7 +161,8 @@ class SharedPartDecoder(keras.layers.Layer):
         x = self.deconv5(x)
         x = self.act5(x)
         x = self.bn5(x, training=training)
-        outputs = self.dropout5(x, training=training)
+        x = self.dropout5(x, training=training)
+        outputs = tf.transpose(x, [0, 2, 3, 1, 4])
 
         return outputs
 
@@ -193,8 +194,11 @@ class LocalizationNet(keras.layers.Layer):
 
     def call(self, inputs, training=False):
 
+        stacked_inputs = inputs[0]
+        summed_inputs = inputs[1]
+
         # processing stacked inputs
-        stacked_x = self.stacked_flat(inputs)
+        stacked_x = self.stacked_flat(stacked_inputs)
         stacked_x = self.stacked_fc1(stacked_x)
         stacked_x = self.stacked_act1(stacked_x)
         stacked_x = self.stacked_dropout1(stacked_x, training=training)
@@ -203,7 +207,7 @@ class LocalizationNet(keras.layers.Layer):
         stacked_x = self.stacked_dropout2(stacked_x, training=training)
 
         # processing summed inputs
-        summed_x = self.summed_fc1(inputs)
+        summed_x = self.summed_fc1(summed_inputs)
         summed_x = self.summed_act1(summed_x)
         summed_x = self.summed_dropout1(summed_x, training=training)
 
@@ -223,6 +227,183 @@ class Resampling(keras.layers.Layer):
     def __init__(self, **kwargs):
         super(Resampling, self).__init__(**kwargs)
 
+    def call(self, inputs):
+
+        # input_fmap has shape (B, num_parts, H, W, D, C)
+        # theta has shape (B, 12*num_parts)
+        input_fmap = inputs[0]
+        theta = inputs[1]
+
+        # batch_grids has shape (B, num_parts, 3, H, W, D)
+        batch_grids = self._affine_grid_generator(input_fmap, theta)
+
+        # x_s, y_s and z_s have shape (B, num_parts, 1, H, W, D)
+        x_s = batch_grids[:, 0, :, :]
+        y_s = batch_grids[:, 1, :, :]
+        z_s = batch_grids[:, 2, :, :]
+
+        # output_fmap has shape (B, num_parts, H, W, D, C)
+        output_fmap = self._trilinear_sampler(input_fmap, x_s, y_s, z_s)
+
+        return output_fmap
+
+    @staticmethod
+    def _affine_grid_generator(input_fmap, theta):
+        """
+        :param input_fmap: the stacked decoded parts in the shape of (B, num_parts, H, W, D, C)
+        :param theta: the output of LocalizationNet. has shape (B, 12*num_parts)
+        :return: affine grid for the input feature map. affine grid has shape (B, num_parts, 3, H, W, D)
+        """
+
+        # get B, num_parts, H, W, D
+        B = tf.shape(input_fmap)[0]
+        num_parts = tf.shape(input_fmap)[1]
+        H = tf.shape(input_fmap)[2]
+        W = tf.shape(input_fmap)[3]
+        D = tf.shape(input_fmap)[4]
+
+        # reshape theta to (B, num_parts, 3, 4)
+        theta = tf.reshape(theta, [B, num_parts, 3, 4])
+
+        # create regular 3D grid, which are the x, y, z coordinates of the output feature map
+        x = tf.linspace(-1.0, 1.0, W)
+        y = tf.linspace(-1.0, 1.0, H)
+        z = tf.linspace(-1.0, 1.0, D)
+        x_t, y_t, z_t = tf.meshgrid(x, y, z)
+
+        # flatten every x, y, z coordinates
+        x_t_flat = tf.reshape(x_t, [-1])
+        y_t_flat = tf.reshape(y_t, [-1])
+        z_t_flat = tf.reshape(z_t, [-1])
+        # x_t_flat has shape (H*W*D,)
+
+        # reshape to (x_t, y_t, z_t, 1), which is homogeneous form
+        ones = tf.ones_like(x_t_flat)
+        sampling_grid = tf.stack([x_t_flat, y_t_flat, z_t_flat, ones])
+        # sampling_grid now has shape (4, H*W*D)
+
+        # repeat the grid num_batch times along axis=0 and num_parts times along axis=1
+        sampling_grid = tf.expand_dims(sampling_grid, axis=0)
+        sampling_grid = tf.expand_dims(sampling_grid, axis=1)
+        sampling_grid = tf.tile(sampling_grid, [B, num_parts, 1, 1])
+        # sampling grid now has shape (B, num_parts, 4, H*W*D)
+
+        # cast to float32 (required for matmul)
+        theta = tf.cast(theta, 'float32')
+        sampling_grid = tf.cast(sampling_grid, 'float32')
+
+        # transform the sampling grid using batch multiply
+        batch_grids = theta @ sampling_grid
+        # batch grid now has shape (B, num_parts, 3, H*W*D)
+
+        # reshape to (B, num_parts, 3, H, W, D)
+        batch_grids = tf.reshape(batch_grids, [B, num_parts, 3, H, W, D])
+
+        return batch_grids
+
+    def _trilinear_sampler(self, input_fmap, x, y, z):
+
+        """
+        :param input_fmap: the stacked decoded parts in the shape of (B, num_parts, H, W, D, C)
+        :param x: x coordinate of input_fmap in the shape of (B, num_parts, 1, H, W, D)
+        :param y: y coordinate of input_fmap in the shape of (B, num_parts, 1, H, W, D)
+        :param z: z coordinate of input_fmap in the shape of (B, num_parts, 1, H, W, D)
+        :return: interpolated volume in the shape of (B, num_parts, H, W, D, C)
+        """
+
+        H = tf.shape(input_fmap)[2]
+        W = tf.shape(input_fmap)[3]
+        D = tf.shape(input_fmap)[4]
+        max_x = tf.cast(W - 1, 'int32')
+        max_y = tf.cast(H - 1, 'int32')
+        max_z = tf.cast(D - 1, 'int32')
+        zero = tf.zeros([], dtype='int32')
+
+        # rescale x/y/z to [0, W-1/H-1/D-1]
+        x = tf.cast(x, 'float32')
+        y = tf.cast(y, 'float32')
+        z = tf.cast(z, 'float32')
+        x = 0.5 * ((x + 1.0) * tf.cast(max_x - 1, 'float32'))
+        y = 0.5 * ((y + 1.0) * tf.cast(max_y - 1, 'float32'))
+        z = 0.5 * ((z + 1.0) * tf.cast(max_z - 1, 'float32'))
+
+        # grab 8 nearest corner points for each (x_i, y_i, z_i) in input_fmap
+        # 2*2*2 combination, so that there are 8 corner points in total
+        x0 = tf.cast(tf.floor(x), 'int32')
+        x1 = x0 + 1
+        y0 = tf.cast(tf.floor(y), 'int32')
+        y1 = y0 + 1
+        z0 = tf.cast(tf.floor(z), 'int32')
+        z1 = z0 + 1
+
+        # clip to range [0, H-1/W-1/D-1] to not violate boundaries
+        x0 = tf.clip_by_value(x0, zero, max_x)
+        x1 = tf.clip_by_value(x1, zero, max_x)
+        y0 = tf.clip_by_value(y0, zero, max_y)
+        y1 = tf.clip_by_value(y1, zero, max_y)
+        z0 = tf.clip_by_value(z0, zero, max_z)
+        z1 = tf.clip_by_value(z1, zero, max_z)
+
+        # x0, x1, y0, y1, z0, z1 have shape (B, num_parts, 1, H, W, D)
+        x0 = tf.squeeze(x0, axis=2)
+        x1 = tf.squeeze(x1, axis=2)
+        y0 = tf.squeeze(y0, axis=2)
+        y1 = tf.squeeze(y1, axis=2)
+        z0 = tf.squeeze(z0, axis=2)
+        z1 = tf.squeeze(z1, axis=2)
+        # x0, x1, y0, y1, z0, z1 now have shape (B, num_parts, H, W, D)
+
+        # get voxel value at corner coords
+        # pay attention to the difference of the ordering between real world coordinates and voxel coordinates
+        c000 = self._get_voxel_value(input_fmap, x0, y1, z0)
+        c001 = self._get_voxel_value(input_fmap, x0, y1, z1)
+        c010 = self._get_voxel_value(input_fmap, x0, y0, z0)
+        c011 = self._get_voxel_value(input_fmap, x0, y0, z1)
+        c100 = self._get_voxel_value(input_fmap, x1, y1, z0)
+        c101 = self._get_voxel_value(input_fmap, x1, y1, z1)
+        c110 = self._get_voxel_value(input_fmap, x1, y0, z0)
+        c111 = self._get_voxel_value(input_fmap, x1, y0, z1)
+        # cxxx has shape (B, num_parts, H, W, D, C)
+
+        # recast as float for delta calculation
+        x0 = tf.cast(x0, 'float32')
+        y1 = tf.cast(y1, 'float32')
+        z0 = tf.cast(z0, 'float32')
+
+        # calculate deltas
+        xd = x - x0
+        yd = y1 - y
+        zd = z - z0
+
+        # compute output (this is the  trilinear interpolation formula in real world coordinate system)
+        output_fmap = c000 * (1 - xd) * (1 - yd) * (1 - zd) + c100 * xd * (1 - yd) * (1 - zd) + \
+                      c010 * (1 - xd) * yd * (1 - zd) + c001 * (1 - xd) * (1 - yd) * zd + \
+                      c101 * xd * (1 - yd) * zd + c011 * (1 - xd) * yd * zd + \
+                      c110 * xd * yd * (1 - zd) + c111 * xd * yd * zd
+        # output feature map now has shape (B, num_parts, H, W, D, C)
+        return output_fmap
+
+    @staticmethod
+    def _get_voxel_value(input_fmap, x, y, z):
+
+        """
+        :param input_fmap: input feature map in the shape of (B, num_parts, H, W, D, C)
+        :param x: x coordinates in the shape of (B, num_parts, H, W, D)
+        :param y: y coordinates in the shape of (B, num_parts, H, W, D)
+        :param z: z coordinates in the shape of (B, num_parts, H, W, D)
+        :return: voxel value in the shape of (B, num_parts, H, W, D, C)
+        """
+
+        # pay attention to the difference of ordering between tensor indexing and voxel coordinates
+        indices = tf.stack([y, x, z], axis=5)
+        return tf.gather_nd(input_fmap, indices, batch_dims=2)
+
+
+class STN(keras.layers.Layer):
+
+    def __init__(self, **kwargs):
+        super(STN, self).__init__(**kwargs)
+
     def call(self, inputs, training=False):
         if training:
             pass
@@ -230,10 +411,10 @@ class Resampling(keras.layers.Layer):
             pass
 
 
-class STN(keras.layers.Layer):
+class Composer(keras.layers.Layer):
 
     def __init__(self, **kwargs):
-        super(STN, self).__init__(**kwargs)
+        super(Composer, self).__init__(**kwargs)
 
     def call(self, inputs, training=False):
         if training:
