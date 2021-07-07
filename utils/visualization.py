79ec52323cf1b0932fcaad001ae27660d59e0ea6
Junwei Zheng <junweizheng.cn@outlook.com>
2021-07-07 12:41:08 +0200

    [modified] | add visualize label and visualize voxel grid functions

--- utils/visualization.py
+++ utils/visualization.py
@@ -2,18 +2,28 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 
-def visualization(voxel, label):
+def visualize_label(label, show_axis=False, show_grid=False):
 
-    # TODO: this function is not fully functional!!!
-
-    color_bar = ['white', 'red', 'green', 'blue', 'pink', 'yellow', 'purple']
+    color_bar = ['white', 'red', 'green', 'blue', 'yellow']
     color = np.empty(label.shape, dtype=str)
     num_parts = np.max(label)
     for part in range(1, num_parts + 1):
         color[label == part] = color_bar[part]
 
     ax = plt.figure().add_subplot(projection='3d')
-    ax.voxels(voxel, edgecolor='k', facecolors=color)
-    plt.axis('off')
-    plt.grid('off')
+    ax.voxels(label > 0, edgecolor='k', facecolors=color)
+    if not show_axis:
+        plt.axis('off')
+    if not show_grid:
+        plt.grid('off')
+    plt.show()
+
+
+def visualize_voxel_grid(voxel_grid, show_axis=False, show_grid=False):
+    ax = plt.figure().add_subplot(projection='3d')
+    ax.voxels(voxel_grid, edgecolor='k')
+    if not show_axis:
+        plt.axis('off')
+    if not show_grid:
+        plt.grid('off')
     plt.show()
